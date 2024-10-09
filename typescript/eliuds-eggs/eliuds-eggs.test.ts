d3a233e03f29fef9af15bee7e3d5b3b8735398a7
Will Adams <bushblade@gmail.com>
2024-10-09 11:43:15 +0100

    complete eluids-eggs

--- typescript/eliuds-eggs/eliuds-eggs.test.ts
+++ typescript/eliuds-eggs/eliuds-eggs.test.ts
@@ -8,19 +8,19 @@ describe('EliudsEggs', () => {
     expect(actual).toEqual(expected)
   })
 
-  test.skip('1 egg', () => {
+  test('1 egg', () => {
     const expected = 1
     const actual = eggCount(16)
     expect(actual).toEqual(expected)
   })
 
-  test.skip('4 eggs', () => {
+  test('4 eggs', () => {
     const expected = 4
     const actual = eggCount(89)
     expect(actual).toEqual(expected)
   })
 
-  test.skip('13 eggs', () => {
+  test('13 eggs', () => {
     const expected = 13
     const actual = eggCount(2000000000)
     expect(actual).toEqual(expected)
--- typescript/eliuds-eggs/eliuds-eggs.ts
+++ typescript/eliuds-eggs/eliuds-eggs.ts
@@ -1,8 +1,9 @@
-//
-// This is only a SKELETON file for the 'Pop Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/**
+ * Convert a Number to binary string and count the '1's
+ * @param {number} displayValue - the number currently displayed
+ * @returns {number}
+ */
 
-export const eggCount = (displayValue: unknown): unknown => {
-  throw new Error('Remove this statement and implement this function')
+export const eggCount = (displayValue: number): number => {
+  return displayValue.toString(2).replace(/[^1]/g, '').length
 }
